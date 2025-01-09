5946f4e95f270d850879d4a5bbb570c263128bd5
Anton Van Assche <vanasscheanton@gmail.com>
2025-01-09 23:52:00 +0100

    [tailscale] Add accept_dns parameter for DNS management

--- site-modules/profile/manifests/tailscale.pp
+++ site-modules/profile/manifests/tailscale.pp
@@ -2,12 +2,16 @@
 #
 # @param authkey
 #   Authentication key for Tailscale.
+# @param accept_dns
+#   Allow Tailscale to manage /etc/resolv.conf.
+#   @see https://tailscale.com/kb/1235/resolv-conf
 #
 # example Basic usage.
 #   include profile::tailscale
 #
 class profile::tailscale (
-  String[1] $authkey,
+  String[1]  $authkey,
+  Boolean    $accept_dns = true,
 ) {
   apt::source { 'tailscale':
     location => 'https://pkgs.tailscale.com/stable/raspbian',
@@ -32,7 +36,7 @@ class profile::tailscale (
   }
 
   exec { 'run_tailscale_up':
-    command     => "/usr/bin/tailscale up --authkey ${authkey}",
+    command     => "/usr/bin/tailscale up --authkey ${authkey} --accept-dns=${accept_dns}",
     provider    => 'shell',
     refreshonly => true,
     unless      => 'test $(/usr/bin/tailscale status | wc -l) -gt 1',
