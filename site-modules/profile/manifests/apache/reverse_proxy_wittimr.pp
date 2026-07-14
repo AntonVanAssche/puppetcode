1ec2243c126465bd8b0fbe17191e97db64f3253d
Anton Van Assche <vanasscheanton@gmail.com>
2026-07-14 23:24:56 +0200

    [wittimr] Make servername a mandatory class parameter

--- site-modules/profile/manifests/apache/reverse_proxy_wittimr.pp
+++ site-modules/profile/manifests/apache/reverse_proxy_wittimr.pp
@@ -1,20 +1,23 @@
 # @summary Configure Apache reverse proxy for wittimr.
 #
+# @example Basic usage.
+#   class { 'profile::apache::reverse_proxy_wittimr':
+#     servername => 'wittimr.local',
+#   }
+#
+# @param servername
+#   The servername to use for the virtual host.
 # @param proxy_dest
 #   The destination to proxy to.
 #
-# @example Basic usage.
-#   include profile::apache::reverse_proxy_wittimr
-#
 class profile::apache::reverse_proxy_wittimr (
-  String[1] $proxy_dest = 'localhost',
+  Stdlib::Fqdn $servername,
+  String[1]    $proxy_dest = 'localhost',
 ) {
-  $_servername = $facts['networking']['domain']
-
-  apache::vhost { "${_servername}_ssl":
+  apache::vhost { "${servername}_ssl":
     ensure              => present,
     docroot             => '/var/www/html',
-    servername          => $_servername,
+    servername          => $servername,
     proxy_preserve_host => true,
     proxy_requests      => false,
     port                => 443,
